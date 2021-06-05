b13d314997c1f6f066941e524ba2eb34cf847dc4
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-06-06 00:21:18 +0300

    Make ns cleanup less brittle

--- cleanup_ns.sh
+++ cleanup_ns.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-for ns in $(ip netns list | cut -d' ' -f1); do
+for ns in $(ip --json netns list | jq -r '.[] | .name'); do
 	for intf in $(ip netns exec "${ns}" ip --json link list | jq -r '.[] | .ifname'); do
 		ip netns exec "${ns}" ip link set "${intf}" down
 		if ! [ "${intf}" = "lo" ]; then
@@ -10,7 +10,7 @@ for ns in $(ip netns list | cut -d' ' -f1); do
 	done
 
 	# Delete veth
-	nsid="$(ip netns list | grep "^${ns}" | sed 's/.*(id: \(.\+\))/\1/')"
+	nsid="$(ip --json netns list | jq --arg name "${ns}" -r '.[] | select(.name == $name) | .id')"
 	if [ -n "${nsid}" ]; then
 		for intf in $(ip --json link list | jq -r --argjson nsid "${nsid}" '.[] | select(.link_netnsid == $nsid) | .ifname'); do
 			ip link del "${intf}"
